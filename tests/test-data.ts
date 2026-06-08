92224f519681c8a320ab2aa4025fb89dd156ebb1
yulitsk <ylitskevich@solvd.com>
2026-06-08 17:45:29 -0400

    Delete old saucedemo tests

--- tests/test-data.ts
+++
@@ -1,7 +0,0 @@
-type Credentials = {
-  username: string;
-  password: string;
-};
-const validUser: Credentials = {username: "standard_user", password: "secret_sauce"};
-const lockedUser: Credentials = {username: "locked_out_user", password: "secret_sauce"};
-export { Credentials, validUser, lockedUser };
